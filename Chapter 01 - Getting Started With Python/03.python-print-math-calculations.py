6a4018fd85865626c16b4b7db9b9d3ee4e1e7224
Mohan Poddar <contactme.mohan@gmail.com>
2020-04-24 01:29:32 +0530

    Modify script 03

--- Chapter 01 - Getting Started With Python/03.python-print-math-calculations.py
+++ Chapter 01 - Getting Started With Python/03.python-print-math-calculations.py
@@ -2,15 +2,19 @@
 
 # print() is a function which is used to display the specified content on the screen.
 
+# Use print() function to perform simple mathematical operations. 
 print(5 + 10)
 
 print()    #Print a blank line
+
 print(12/3 + 14)
 
 print()    #Print a blank line
+
 print (8/2 * 6 + 7)
 
 print()    #Print a blank line
+
 print(22/7)
 
 print()    #Print a blank line
