7cdfbe9eb73c2c442217fd25be50299f0bcc558f
Mohan Poddar <contactme.mohan@gmail.com>
2020-04-28 01:33:37 +0530

    Adding Chapter - 03 Python Conditions and Decision Making

---
+++ Chapter 03 - Python Conditions And Decision Making/03.python-conditions-and-decision-making-if-else-statement.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+# Understanding Python Conditions And Decision Making - if-else statement
+
+import time
+
+#:-------------------------------------------- Prog. - 01 --------------------------------------------:
+print()
+print("Running programme : Prog. - 01")
+
+x = 15
+print("The value of x is",x)
+
+if x > 10:
+   print("x is greater than 10")
+else:
+   print("x is less than 10" )
+
+time.sleep(1)
+print()
+
+x = 7
+print("The new value of x is",x)
+
+if x > 10:
+   print("x is greater than 10")
+
+else:
+   print("x is less than 10")
+
+time.sleep(2)
+
+#:-------------------------------------------- Prog. - 02 --------------------------------------------:
+print()
+print("Running programme : Prog. - 02")
+
+Age = 25
+if Age >= 18:
+   print("If you are", Age, "years old then eligible to caste Vote!" + '\n')           # Printing if statement.
+else:
+   print("If you are", Age, "years old then not eligible to caste Vote." + '\n')
+
+time.sleep(1)
+
+Age = 12
+if Age >= 18:
+   print("If you are", Age, "years old then eligible to caste Vote!" + '\n')
+else:
+   print("If you are", Age, "years old then not eligible to caste Vote." + '\n')       # Printing else statement.
+
+time.sleep(2)
+
+#:-------------------------------------------- Prog. - 03 --------------------------------------------:
+print("Running programme : Prog. - 03")
+print("Check Your Eligibility to Caste Vote:")
+Name = str(input("Enter Your Name: "))
+Age = int(input("Enter Your Age: "))
+
+if Age >= 18:
+   print('\n' + "Hello", Name, ", you are eligible to caste Vote!" + '\n')
+else:
+   print('\n' + "Sorry", Name, ", you are not eligible to caste Vote!" + '\n')
+time.sleep(1)
+
+#:-------------------------------------------- Prog. - 04 --------------------------------------------:
+print()
+print("Running programme : Prog. - 04")
+print("Check Your Eligibility for Gratuity:")
+Name = str(input("Enter Your Name: "))
+Syears = int(input("Enter Your Years of Service in Current Job : "))
+
+if Syears >= 5:
+   print('\n' + "Congratulations!", Name, ", you are eligible for Gratuity!!" + '\n')
+else:
+   print('\n' + "Ohh..", Name, "you are not eligible for Gratuity." + '\n')
+time.sleep(1)
