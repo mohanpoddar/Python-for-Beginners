60ede5b18c0265a83592ca9f81b23b8040852914
Mohan Poddar <contactme.mohan@gmail.com>
2020-05-03 18:43:12 +0530

    Adding Chapter Python Loops

---
+++ Chapter 04 - Python Loops/01.python-for-loop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Understanding Python loops - loop
+
+import time
+
+#:-------------------------------------------- Prog. - 01 --------------------------------------------:
+for country in 'India':
+    print("Welcome to", country)   
+print()
+
+
