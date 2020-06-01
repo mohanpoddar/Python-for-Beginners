0731f33dcd5860e7e0a6bc50424f2ebc40bccd3b
Mohan Poddar <contactme.mohan@gmail.com>
2020-06-02 02:40:31 +0530

    Add New Chapter

---
+++ Chapter 10 - Python if __name__ == '__main__'/01.python-first-scenerio/python_first_module.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Python Concepts - if __name__ == '__main__'
+
+#:-------------------------------------------- Prog. - 01 --------------------------------------------:
+
+print ("First Module's Name: {}".format(__name__))      # Run directory from this file.
+
+'''
+What happend here?
+Whenever a python programme runs then it set some special variabe.
+__name__ is that special variable.
+python sets it's __name__ == __main__
+'''
---
+++ Chapter 10 - Python if __name__ == '__main__'/04.python-4th-scenerio/python_first_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Python Concepts - if __name__ == '__main__'
+
+#:-------------------------------------------- Prog. - 01 --------------------------------------------:
+if __name__ == '__main__':
+    print("Run Directly")
+
+elif __name__ != '__main__':
+    print("Run from Import")
+
+else:
+    print("Neither Diectly nor Import")
